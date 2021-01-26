4630b42fa4866d82436d25a9cbd29ea0cd5d91fb
TristanEvangelista <tristan.evangelista@obf.ateneo.edu>
2021-01-26 16:54:45 +0700

    added input output

--- Test.java
+++ Test.java
@@ -1,6 +1,12 @@
+import java.io.*;
+import java.util.*;
+
 public class Test{
     //"Test" is the name of the java file
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        String greeting = "Hello World";
+        System.out.println(greeting);
+        scanner.close();
     }
 }
